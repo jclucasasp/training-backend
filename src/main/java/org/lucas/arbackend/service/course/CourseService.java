c981c906fff2777c24a07bf8b903427ad70878b2
jc <jclucasasp@gmail.com>
2026-01-29 18:19:53 +0200

    Update CourseService to reflect multi tenant

--- src/main/java/org/lucas/arbackend/service/course/CourseService.java
+++ src/main/java/org/lucas/arbackend/service/course/CourseService.java
@@ -6,14 +6,19 @@ import org.lucas.arbackend.dto.course.CourseMapper;
 import org.lucas.arbackend.dto.course.helper.CourseRequest;
 import org.lucas.arbackend.dto.course.helper.CourseResponse;
 import org.lucas.arbackend.entity.course.Course;
+import org.lucas.arbackend.entity.student.Student;
 import org.lucas.arbackend.repository.course.CourseRepository;
+import org.lucas.arbackend.repository.student.StudentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CourseService {
 
     private final CourseRepository courseRepository;
+    private final StudentRepository studentRepository;
     private final CourseMapper mapper;
 
     public CourseResponse createCourse(CourseRequest request) {
@@ -21,14 +26,16 @@ public class CourseService {
         return mapper.toResponse(courseRepository.save(course));
     }
 
-    public CourseResponse getCourseById(Long id) {
-        return mapper.toResponse(courseRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Course not found")));
-    }
+     public List<CourseResponse> getCoursesForStudent(String studentNumber) {
+        // Find the student to get their related org_id
+        Student student = studentRepository.findByStudentNumber(studentNumber)
+                .orElseThrow(() -> new EntityNotFoundException("Student not found"));
 
-    public CourseResponse getCourseByName(String courseName) {
-        return mapper.toResponse(courseRepository.findByName(courseName)
-                .orElseThrow(() -> new EntityNotFoundException("Course not found")));
+        // Query courses filtered by that org_id
+        return courseRepository.findAllByOrganisationId(student.getOrganisation().getId())
+                .stream()
+                .map(mapper::toResponse)
+                .toList();
     }
 
     public CourseResponse updateCourse(Long id, CourseRequest request) {
@@ -43,7 +50,6 @@ public class CourseService {
     }
 
     public void deleteCourse(Long id) {
-
         courseRepository.deleteById(id);
     }
 }
